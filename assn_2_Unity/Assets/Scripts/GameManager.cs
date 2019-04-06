388019b5f749d0d953bcc87f7076907bed6926a8
Yugantar777 <yarya@cs.iitr.ac.in>
2019-04-06 20:41:21 +0530

    New assignment

---
+++ assn_2_Unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public float restartDelay = 1f;
+    public GameObject comlpeteLevelUI;
+
+    public void CompleteLevel()
+    {
+        comlpeteLevelUI.SetActive(true);
+    }
+
+  public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
