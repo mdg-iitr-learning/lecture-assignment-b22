7425cc989d2592fd7f6e12ae68c0853701075172
Nitish145 <royalnitish21@gmail.com>
2019-04-15 17:32:12 +0530

    Unity_assignment

---
+++ Unity_VR_Assignment/Assets/Scripts/reticleTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reticleTest : MonoBehaviour
+{ 
+    public void RandomlyTeleport()
+    {
+        gameObject.transform.position = new Vector3(GetRandomCoordinate(), Random.Range(.5f, 2), GetRandomCoordinate());
+    }
+
+    private float GetRandomCoordinate()
+    {
+        var cordinate = Random.Range(-7f, 7f);
+        while(cordinate > -1.5f && cordinate < 1.5f)
+        {
+            cordinate = Random.Range(-5, 5);
+        }
+        return cordinate;
+    }
+}
